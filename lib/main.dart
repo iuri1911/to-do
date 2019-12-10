134de91a51f89e15923b63bef02e4926a7451efc
iuri ribeiro <iuriribeiro@MBP-de-iuri.bbrouter>
2019-12-10 17:24:50 -0300

    major updates

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: Home(),
-  ))
+  ));
 }
 
 class Home extends StatefulWidget {
@@ -15,13 +15,52 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  
-  List _toDoList = [];
+  List _toDoList = ["igorgag","minha pika", "cuckold"];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("lista de tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "nova tarefa",
+                    labelStyle: TextStyle(color: Colors.blueAccent),
+                  ),
+                ),
+                ),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  child: Text("ADD"),
+                  textColor: Colors.white,
+                  onPressed: (){},
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.only(top: 10.0),
+              itemCount: _toDoList.length,
+              itemBuilder: (context, index){
+                return ListTile(
+                  title: Text(_toDoList[index]),
+                );
+              }
+              ),
+          )
+        ],
+      ),
     );
   }
 
@@ -32,14 +71,14 @@ class _HomeState extends State<Home> {
 
   Future<File> _SaveData() async {
     String data = json.encode(_toDoList);
-    final file = await _getFile()
+    final file = await _getFile();
     return file.writeAsString(data);
   }
 
-  Future<String> _readData () async {
+  Future<String> _readData() async {
     try {
-     final file = await _getFile();
-     return file.readAsString();
+      final file = await _getFile();
+      return file.readAsString();
     } catch (e) {
       return null;
     }
