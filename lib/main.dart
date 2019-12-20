62838c8e1868c2b5d38e49c3c85791e873d4a568
Iuri <theozzybr@gmail.com>
2019-12-20 01:50:19 -0300

    save on json and other improviments

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _toDoList = ["igorgag", "minha pika", "cuckold"];
+  final _toDoController = TextEditingController();
+
+  List _toDoList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+      _SaveData();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,6 +57,7 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 Expanded(
                   child: TextField(
+                    controller: _toDoController,
                     decoration: InputDecoration(
                       labelText: "nova tarefa",
                       labelStyle: TextStyle(color: Colors.blueAccent),
@@ -42,7 +68,9 @@ class _HomeState extends State<Home> {
                   color: Colors.blueAccent,
                   child: Text("ADD"),
                   textColor: Colors.white,
-                  onPressed: () {},
+                  onPressed: () {
+                    _addToDo();
+                  },
                 )
               ],
             ),
@@ -52,8 +80,19 @@ class _HomeState extends State<Home> {
                 padding: EdgeInsets.only(top: 10.0),
                 itemCount: _toDoList.length,
                 itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(_toDoList[index]),
+                  return CheckboxListTile(
+                    title: Text(_toDoList[index]["title"]),
+                    value: _toDoList[index]["ok"],
+                    secondary: CircleAvatar(
+                      child: Icon(
+                          _toDoList[index]["ok"] ? Icons.check : Icons.error),
+                    ),
+                    onChanged: (c) {
+                      setState(() {
+                        _toDoList[index]["ok"] = c;
+                        _SaveData();
+                      });
+                    },
                   );
                 }),
           )
